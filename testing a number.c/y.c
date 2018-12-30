d1c9851f355384e2c7264d833556b8191a7db8cd
yashg4148 <46264580+yashg4148@users.noreply.github.com>
2018-12-30 22:35:20 +0530

    Create y.c

---
+++ testing a number.c/y.c
@@ -0,0 +1,14 @@
+/* To test a given number is positive,negative or zero*/
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a>0)
+printf("positive");
+else if(a==0)
+printf("zero");
+else
+printf("negative");
+return 0;
+}
