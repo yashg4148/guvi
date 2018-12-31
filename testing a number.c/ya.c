21a9684e84ada9960475a46759fbe1e5b23dbebe
yashg4148 <46264580+yashg4148@users.noreply.github.com>
2018-12-31 12:29:02 +0530

    Update ya.c

--- testing a number.c/ya.c
+++ testing a number.c/ya.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 int main()
 {
-int n;
-scanf("%d",&n);
-if(n>0)
-{
-if(n%2==0)
-printf("even");
-else
-printf("odd");
-}
-else
-printf("invalid");
-return 0;
+  int n;
+  scanf("%d",&n);
+  if(n>0)
+  {
+    
+    if(n%2==0)
+      printf("even");
+    else 
+      printf("odd");
+  }
+  else
+    printf("invalid");
+  return 0;
 }
