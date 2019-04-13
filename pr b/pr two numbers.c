22ab2cc3c84105c6d15e5f5cd3d74f7856af88f5
yashg4148 <46264580+yashg4148@users.noreply.github.com>
2019-04-13 21:31:07 +0530

    Update and rename pr two numbers to pr two numbers.c

---
+++ pr b/pr two numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int i,m,n,c,j;
+scanf("%d%d",&m,&n);
+ for(i=m+1;i<n;i++)
+ {
+  c=0;
+   for(j=2;j<i;j++)
+   {
+    if(i%j==0)
+    c++;
+    break;
+   }
+   if(c==0)
+   printf("%d",i);
+ }
+return 0;
+}
