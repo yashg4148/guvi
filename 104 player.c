ca973b93775813c3047170562ef275629b0d2a1d
yashg4148 <46264580+yashg4148@users.noreply.github.com>
2019-05-18 16:58:44 +0530

    Create 104 player.c

---
+++ 104 player.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int i,*p,n,s=0;
+scanf("%d",&n);
+p=(int*)malloc(n*sizeof(int));
+
+for(i=0;i<n;i++)
+scanf("%d",(p+i));
+
+for(i=0;i<n-1;i++)
+s=s+(p[i]+p[i+1]);
+
+printf("%d",s);
+return 0;
+}
